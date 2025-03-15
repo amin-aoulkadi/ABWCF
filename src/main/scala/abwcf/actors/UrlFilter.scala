2a2007627dd152a98915794481bac5e1cfabf67b
amin-aoulkadi <65405621+amin-aoulkadi@users.noreply.github.com>
2025-03-15 21:56:13 +0100

    Implemented URL filtering

--- src/main/scala/abwcf/actors/UrlFilter.scala
+++ src/main/scala/abwcf/actors/UrlFilter.scala
@@ -1,24 +1,44 @@
 package abwcf.actors
 
-import org.apache.pekko.actor.typed.{ActorRef, Behavior}
 import org.apache.pekko.actor.typed.scaladsl.Behaviors
+import org.apache.pekko.actor.typed.{ActorRef, Behavior}
+
+import scala.jdk.CollectionConverters.*
+import scala.util.matching.Regex
 
 /**
- * Applies black- and whitelist filtering to discard URLs that should not be crawled.
+ * Uses configurable regular expressions to filter out URLs that should not be crawled.
  * 
  * There should be one [[UrlFilter]] actor per node.
  * 
  * This actor is stateless.
+ * 
+ * @see [[java.util.regex.Pattern]]
  */
 object UrlFilter {
   sealed trait Command
   case class Filter(url: String) extends Command
 
   def apply(pageManager: ActorRef[PageManager.Command]): Behavior[Command] = Behaviors.setup(context => {
+    val config = context.system.settings.config
+    
+    val mustMatch = config.getStringList("abwcf.url-filter.must-match")
+      .asScala
+      .map(Regex(_))
+    
+    val mustNotMatch = config.getStringList("abwcf.url-filter.must-not-match")
+      .asScala
+      .map(Regex(_))
+
     Behaviors.receiveMessage({
       case Filter(url) =>
-        //TODO: Implement URL filtering.
-        pageManager ! PageManager.Spawn(url)
+        val existsRequiredMatch = mustMatch.exists(_.matches(url))
+        val existsForbiddenMatch = mustNotMatch.exists(_.matches(url))
+
+        if (existsRequiredMatch && !existsForbiddenMatch) {
+          pageManager ! PageManager.Spawn(url)
+        }
+        
         Behaviors.same
     })
   })
