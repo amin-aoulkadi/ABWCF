6e400134fa297724c1dcf39ed222ab1b823ebada
amin-aoulkadi <65405621+amin-aoulkadi@users.noreply.github.com>
2025-03-24 15:55:42 +0100

    Added graceful passivation for HostQueue and Page

--- src/main/scala/abwcf/actors/HostQueue.scala
+++ src/main/scala/abwcf/actors/HostQueue.scala
@@ -1,68 +1,98 @@
 package abwcf.actors
 
 import org.apache.pekko.actor.typed.receptionist.{Receptionist, ServiceKey}
-import org.apache.pekko.actor.typed.scaladsl.Behaviors
+import org.apache.pekko.actor.typed.scaladsl.{ActorContext, Behaviors}
 import org.apache.pekko.actor.typed.{ActorRef, ActorSystem, Behavior}
 import org.apache.pekko.cluster.sharding.typed.ShardingEnvelope
 import org.apache.pekko.cluster.sharding.typed.scaladsl.{ClusterSharding, Entity, EntityTypeKey}
 
 import java.time.{Duration, Instant}
 import scala.collection.immutable.Queue
+import scala.concurrent.duration.FiniteDuration
+import scala.jdk.DurationConverters.*
 
 /**
  * Manages the crawl delay for a host.
  *
  * There should be exactly one [[HostQueue]] actor per crawled host.
  *
- * This actor is stateful, sharded and registered with the receptionist.
+ * This actor is stateful, sharded, gracefully passivated and registered with the receptionist.
  *
  * Entity ID: Domain name or IP address of the host.
  */
-object HostQueue { //TODO: Handle (automatic) passivation and handle HostQueues being moved to a different node!
+object HostQueue { //TODO: HostQueues are not persisted so they reset after shard rebalancing.
   val TypeKey: EntityTypeKey[Command] = EntityTypeKey("HostQueue")
   val HQServiceKey: ServiceKey[Command] = ServiceKey("HostQueue")
 
   sealed trait Command
   case class Enqueue(url: String, crawlDepth: Int) extends Command
   case class GetHead(replyTo: ActorRef[Reply]) extends Command
+  private case object Passivate extends Command
 
   sealed trait Reply
   case class Head(url: String, crawlDepth: Int) extends Reply
   case object Unavailable extends Reply
 
-  def apply(): Behavior[Command] = Behaviors.setup(context => {
+  def apply(shard: ActorRef[ClusterSharding.ShardCommand]): Behavior[Command] = Behaviors.setup(context => {
     val config = context.system.settings.config
     val crawlDelay = config.getDuration("abwcf.host-queue.crawl-delay")
+    val receiveTimeout = config.getDuration("abwcf.host-queue.passivation-receive-timeout").toScala
 
     context.system.receptionist ! Receptionist.Register(HQServiceKey, context.self)
 
-    new HostQueue(crawlDelay).hostQueue(Queue.empty, Instant.MIN)
+    new HostQueue(crawlDelay, receiveTimeout, shard, context).emptyQueue(Instant.MIN)
   })
 
   def getShardRegion(system: ActorSystem[?]): ActorRef[ShardingEnvelope[Command]] = {
-    ClusterSharding(system).init(Entity(TypeKey)(_ => HostQueue()))
+    ClusterSharding(system).init(Entity(TypeKey)(entityContext => HostQueue(entityContext.shard)))
   }
 }
 
-private class HostQueue private (crawlDelay: Duration) {
+private class HostQueue private (crawlDelay: Duration,
+                                 receiveTimeout: FiniteDuration,
+                                 shard: ActorRef[ClusterSharding.ShardCommand],
+                                 context: ActorContext[HostQueue.Command]) {
   import HostQueue.*
 
-  private def hostQueue(urls: Queue[(String, Int)], crawlDelayEnd: Instant): Behavior[Command] = Behaviors.receiveMessage({
-    case Enqueue(url, crawlDepth) =>
-      hostQueue(urls.enqueue((url, crawlDepth)), crawlDelayEnd)
+  private def queue(urls: Queue[(String, Int)], crawlDelayEnd: Instant): Behavior[Command] = {
+    context.cancelReceiveTimeout() //Disable passivation. Non-empty HostQueues should not be passivated.
 
-    case GetHead(replyTo) if urls.nonEmpty && Instant.now.isAfter(crawlDelayEnd) =>
-      val (head, tail) = urls.dequeue
-      replyTo ! Head(head._1, head._2)
-  
-      if (tail.isEmpty) {
-        Behaviors.stopped //TODO: What about messages that are already in or in flight to the inbox of this actor?
-      } else {
-        hostQueue(tail, Instant.now.plus(crawlDelay))
-      }
-        
-    case GetHead(replyTo) =>
-      replyTo ! Unavailable
-      Behaviors.same
-  })
+    Behaviors.receiveMessage({
+      case Enqueue(url, crawlDepth) => queue(urls.enqueue((url, crawlDepth)), crawlDelayEnd)
+
+      case GetHead(replyTo) if Instant.now.isAfter(crawlDelayEnd) =>
+        val (head, tail) = urls.dequeue
+        replyTo ! Head(head._1, head._2)
+
+        if (tail.isEmpty) {
+          emptyQueue(Instant.now.plus(crawlDelay))
+        } else {
+          queue(tail, Instant.now.plus(crawlDelay))
+        }
+
+      case GetHead(replyTo) =>
+        replyTo ! Unavailable
+        Behaviors.same
+
+      case Passivate =>
+        context.log.info("Skipping unexpected message {}", Passivate)
+        Behaviors.same
+    })
+  }
+
+  private def emptyQueue(crawlDelayEnd: Instant): Behavior[Command] = {
+    context.setReceiveTimeout(receiveTimeout, Passivate) //Enable passivation. Empty HostQueues can be passivated (but not immediately as there may still be messages in the mailbox).
+
+    Behaviors.receiveMessage({
+      case Enqueue(url, crawlDepth) => queue(Queue((url, crawlDepth)), crawlDelayEnd)
+
+      case GetHead(replyTo) =>
+        replyTo ! Unavailable
+        Behaviors.same
+
+      case Passivate =>
+        shard ! ClusterSharding.Passivate(context.self)
+        Behaviors.same
+    })
+  }
 }
--- src/main/scala/abwcf/actors/Page.scala
+++ src/main/scala/abwcf/actors/Page.scala
@@ -8,13 +8,14 @@ import org.apache.pekko.persistence.typed.scaladsl.{Effect, EventSourcedBehavior
 import org.apache.pekko.persistence.typed.{PersistenceId, RecoveryCompleted}
 
 import java.net.URI
+import scala.jdk.DurationConverters.*
 
 /**
  * Represents a page to be crawled.
  *
  * There should be exactly one [[Page]] actor per page. [[Page]] actors should be managed by a [[PageManager]] actor.
  *
- * This actor is stateful, sharded and event sourced.
+ * This actor is stateful, sharded, gracefully passivated and event sourced.
  *
  * Entity ID: URL of the page.
  */
@@ -26,16 +27,29 @@ object Page {
   case object Success extends Command
   case object Redirect extends Command
   case object Error extends Command
+  private case object Passivate extends Command
 
   private trait Event
   private case class Discovered(url: String, crawlDepth: Int) extends Event
   private case object Processed extends Event
 
-  def apply(url: String): Behavior[Command] = Behaviors.setup(context => {
+  def apply(url: String, shard: ActorRef[ClusterSharding.ShardCommand]): Behavior[Command] = Behaviors.setup(context => {
+    val config = context.system.settings.config
+    val receiveTimeout = config.getDuration("abwcf.page.passivation-receive-timeout").toScala
     val hostQueueShardRegion = HostQueue.getShardRegion(context.system)
+    
+    context.setReceiveTimeout(receiveTimeout, Passivate)
 
     /**
-     * Adds the URL to a HostQueue so that it can be fetched.
+     * Tells the parent shard to passivate this entity and returns [[Effect.none]].
+     */
+    def passivate(): Effect[Event, State] = {
+      shard ! ClusterSharding.Passivate(context.self)
+      Effect.none
+    }
+
+    /**
+     * Adds the URL to a [[HostQueue]] so that it can be fetched.
      */
     def addToHostQueue(url: String, crawlDepth: Int): Unit = {
       val host = URI(url).getHost
@@ -66,6 +80,7 @@ object Page {
       override def applyCommand(command: Command): Effect[Event, State] = command match {
         case Discover(_, _) => Effect.none //The crawler can discover the same page multiple times, but it doesn't need to fetch the same page multiple times.
         case Success | Redirect | Error => Effect.persist(Processed)
+        case Passivate => passivate()
       }
 
       override def applyEvent(event: Event): State = event match {
@@ -75,8 +90,9 @@ object Page {
     }
 
     case object ProcessedPage extends State{
-      override def applyCommand(command: Command): Effect[Event, State] = {
-        Effect.none
+      override def applyCommand(command: Command): Effect[Event, State] = command match {
+        case Passivate => passivate()
+        case _ => Effect.none
       }
 
       override def applyEvent(event: Event): State = {
@@ -97,6 +113,6 @@ object Page {
   })
 
   def getShardRegion(system: ActorSystem[?]): ActorRef[ShardingEnvelope[Command]] = {
-    ClusterSharding(system).init(Entity(TypeKey)(entityContext => Page(entityContext.entityId)))
+    ClusterSharding(system).init(Entity(TypeKey)(entityContext => Page(entityContext.entityId, entityContext.shard)))
   }
 }
