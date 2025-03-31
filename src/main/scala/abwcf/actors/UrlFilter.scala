a4fbc4b478b4a351af1d8e793d59eeb0673b6427
amin-aoulkadi <65405621+amin-aoulkadi@users.noreply.github.com>
2025-03-31 11:43:15 +0200

    Added maximum URL length

--- src/main/scala/abwcf/actors/UrlFilter.scala
+++ src/main/scala/abwcf/actors/UrlFilter.scala
@@ -8,12 +8,12 @@ import scala.jdk.CollectionConverters.*
 import scala.util.matching.Regex
 
 /**
- * Uses configurable regular expressions to filter out URLs that should not be crawled.
- * 
+ * Filters out URLs that should not be crawled based on URL length and regular expressions.
+ *
  * There should be one [[UrlFilter]] actor per node.
- * 
+ *
  * This actor is stateless.
- * 
+ *
  * @see [[java.util.regex.Pattern]]
  */
 object UrlFilter {
@@ -22,24 +22,27 @@ object UrlFilter {
 
   def apply(pageManager: ActorRef[PageManager.Command]): Behavior[Command] = Behaviors.setup(context => {
     val config = context.system.settings.config
-    
+    val maxUrlLength = config.getInt("abwcf.url-filter.max-url-length")
+
     val mustMatch = config.getStringList("abwcf.url-filter.must-match")
       .asScala
       .map(Regex(_))
-    
+
     val mustNotMatch = config.getStringList("abwcf.url-filter.must-not-match")
       .asScala
       .map(Regex(_))
 
     Behaviors.receiveMessage({
       case Filter(page) =>
-        val existsRequiredMatch = mustMatch.exists(_.matches(page.url))
-        val existsForbiddenMatch = mustNotMatch.exists(_.matches(page.url))
+        if (page.url.length <= maxUrlLength) {
+          val existsRequiredMatch = mustMatch.exists(_.matches(page.url))
+          val existsForbiddenMatch = mustNotMatch.exists(_.matches(page.url))
 
-        if (existsRequiredMatch && !existsForbiddenMatch) {
-          pageManager ! PageManager.Discover(page)
+          if (existsRequiredMatch && !existsForbiddenMatch) {
+            pageManager ! PageManager.Discover(page)
+          }
         }
-        
+
         Behaviors.same
     })
   })
